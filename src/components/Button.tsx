f4343e8d38efe50e29d8073321803f6bc30e81e5
Vincenzo Magnano <vincenzomagnano@Host-008.homenet.telecomitalia.it>
2025-05-06 19:05:37 +0200

    finishing 3d models and implements counter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,23 @@ interface ButtonProps {
 
 const Button = ({ text, className = "", id, href = "#" }: ButtonProps) => {
   return (
-    <a href={href} id={id} className={`${className} cta-wrapper`}>
+    <a 
+    onClick={(e) => {
+      e.preventDefault();
+      const target = document.getElementById('counter')
+
+      if ( target && id){
+        const offset = window.innerHeight * 0.15;
+
+        const top = target.getBoundingClientRect().top + window.scrollY - offset;
+
+        window.scrollTo({top, behavior: 'smooth'})
+      }
+    }}
+    href={href} 
+    id={id} 
+    className={`${className} cta-wrapper`}
+    >
       <div className="cta-button group">
         <div className="bg-circle" />
         <p className="text">{text}</p>
